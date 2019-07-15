44c4312c5fdcf3575163daaf09840b6c89efc16d
dongh9508 <dongh9508@gmail.com>
2019-07-15 20:54:03 +0900

    src update

---
+++ src/main/java/com/matching/MatchingApplication.java
@@ -0,0 +1,21 @@
+package com.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingApplication.class, args);
+    }
+
+
+    @GetMapping("/")
+    public String list() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/matching/MatchingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matching;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatchingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
